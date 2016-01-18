c8be6e6b498df2a4799a6b5f609baab1f4c743d7
Justin Bitely <jbitely@gmail.com>
2016-01-18 14:58:55 -0500

    Implemented HoneyMakerBee subclass with inheritance from Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,10 @@
 var HoneyMakerBee = function() {
+  Bee.call();
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = new Bee();
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
 
