deb698b50d90dd66962d0ca181e9c6e4df3b870d
Justin Bitely <jbitely@gmail.com>
2016-01-18 15:08:50 -0500

    Implemented properties for RetiredForagerBee subclass with inheritance from ForagerBee.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,10 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call();
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = new ForagerBee();
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
