d2f8f3f02a85a9c1c61c046e821263183e3f4ca8
Justin Bitely <jbitely@gmail.com>
2016-01-18 15:04:31 -0500

    Implemented basic ForagerBee subclass properties with inheritance from Bee.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
 var ForagerBee = function() {
+  Bee.call();
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = new Bee();
+ForagerBee.prototype.constructor = ForagerBee;
